a26f9aeaafd750cf66947564894d573d56699447
prajwal cn <127007794+prajwal-cn@users.noreply.github.com>
2023-04-01 15:05:58 +0530

    Update model.py

--- Src/model.py
+++ Src/model.py
@@ -1,1 +1,18 @@
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+def train_model(X_train, y_train):
+    """
+    Train a Random Forest Classifier on the training set.
+    """
+    rf = RandomForestClassifier()
+    rf.fit(X_train, y_train)
+    return rf
+
+def save_model(model, file_path):
+    """
+    Save the trained model as a pickle file.
+    """
+    with open(file_path, 'wb') as f:
+        pickle.dump(model, f)
 
