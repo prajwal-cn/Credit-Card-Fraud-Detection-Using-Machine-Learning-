a902872d62d31d3a61feca41b7955378d01ff84b
prajwal cn <127007794+prajwal-cn@users.noreply.github.com>
2023-04-01 15:06:11 +0530

    Update data.py

--- Src/data.py
+++ Src/data.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def load_data(file_path):
+    """
+    Load the credit card fraud detection dataset from a CSV file.
+    """
+    df = pd.read_csv(file_path)
+    return df
+
+def split_data(df, test_size=0.2):
+    """
+    Split the dataset into train and test sets.
+    """
+    X = df.drop(['Class'], axis=1)
+    y = df['Class']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
 
