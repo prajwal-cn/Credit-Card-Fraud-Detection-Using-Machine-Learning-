8e946b6f81776c9b544d85be8f390e7644e4b616
prajwal cn <127007794+prajwal-cn@users.noreply.github.com>
2023-04-01 15:05:44 +0530

    Update visualization.py

--- Src/visualization.py
+++ Src/visualization.py
@@ -1,1 +1,30 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score
+
+def plot_confusion_matrix(y_true, y_pred):
+    """
+    Plot a confusion matrix for the model's predictions.
+    """
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt='g')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+def plot_roc_curve(y_true, y_pred_proba):
+    """
+    Plot a ROC curve for the model's predictions.
+    """
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred_proba[:, 1])
+    auc_score = roc_auc_score(y_true, y_pred_proba[:, 1])
+    plt.plot(fpr, tpr, label=f'AUC = {auc_score:.2f}')
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc='lower right')
+    plt.show()
 
